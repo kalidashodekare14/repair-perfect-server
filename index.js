1ed1dad3d906d1f7a8ecf64f7758bdb01365d59e
Kalidash Odekare <shaktiadhikari49@gmail.com>
2024-05-13 16:22:16 +0600

    Assignment Server Project Update

--- index.js
+++ index.js
@@ -98,6 +98,13 @@ async function run() {
             res.send(result)
         })
 
+        app.get('/detail/:id', async(req, res) => {
+            const id = req.params.id
+            const query = {_id: new ObjectId(id)}
+            const result = await repairServices.findOne(query)
+            res.send(result)
+        })
+
 
         app.get('/service_details/:id', async (req, res) => {
             const id = req.params.id
@@ -153,7 +160,7 @@ async function run() {
             res.send(purchase)
         })
 
-       
+
 
         app.get('/service_to_do/:email', async (req, res) => {
             const email = req.params.email
